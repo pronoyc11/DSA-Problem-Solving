adc64059ef4daa903031b80f65d0bcde099a2118
pronoyc11 <pronoyc11@gmail.com>
2026-04-15 16:26:35 +0600

    final test

---
+++ Arrays/beautiful_matrix.cpp
@@ -0,0 +1,38 @@
+
+/*
+Problem: Beautiful matrix
+Platform: Codeforces
+Difficulty: Easy
+Topic: Prefix Sum
+Link:https://codeforces.com/problemset/problem/263/A
+Status: Solved
+*/
+
+
+
+
+#include<iostream>
+
+using namespace std;
+
+
+int main() {
+
+    int p,q;
+
+    for(int i= 1; i<=5; i++) {
+        for(int j=1; j<=5; j++) {
+            int num;
+            cin>>num;
+
+            if(num == 1) {
+                p= i;
+                q= j;
+            }
+        }
+    }
+ int moves = abs(3-p) + abs(3-q) ;
+
+ cout<< moves<<endl;
+    return 0;
+}
