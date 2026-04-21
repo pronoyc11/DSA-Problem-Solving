73676ac3bcdf0655f6539345a82c0c8dd0147716
pronoyc11 <pronoyc11@gmail.com>
2026-04-21 22:47:15 +0600

    emni

---
+++ Arrays/two_sum.py
@@ -0,0 +1,23 @@
+
+'''
+Problem: TwoSum
+Platform: LeetCode
+Difficulty: medium
+Topic: Implementation
+Link: https://leetcode.com/problems/two-sum/
+Status: Solved
+
+'''
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        lookup = {}
+        i=0
+        for num in nums:
+            difference = target-num
+            if(difference not in lookup):
+                lookup[num] = lookup.get(num,0)+1
+            else:
+                return [nums.index(difference),i]
+            i +=1
+        return False
